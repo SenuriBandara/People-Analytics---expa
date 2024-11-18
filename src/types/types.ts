9f0caa582de00cd0266d6318e6aaca53cfc87aee
Senuri Chathurangi <155548955+SenuriBandara@users.noreply.github.com>
2024-11-18 17:03:05 +0530

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,20 @@
+export type Filter = {
+	entity: string;
+	from: string;
+	to: string;
+	age: number;
+	person_profile.selected_programmes: string[];
+	languages : string[];
+	skills : string[];
+	referral_type : string[];
+	is_aiesecer : Boolean;
+	lc_alignment : string;
+	nationalities : string[];
+	campaignTag?: number;
+		}
+}
 
+export type Office = {
+	id: string;
+	name: string;
+}
